a08594dc691566238ebbf8ea5655457267a1bf79
Tobias Prandini <tobiasprandini@gmail.com>
2025-03-21 19:49:08 -0300

    Random Password Generator

---
+++ random-password-app/script.js
@@ -0,0 +1,32 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+async function copyPassword() {
+    try {
+        await navigator.clipboard.writeText(passwordBox.value);
+        alert("Senha copiada com sucesso!");
+    } catch (err) {
+        alert("Erro ao copiar a senha. Por favor, tente novamente.");
+        console.error("Erro ao copiar:", err);
+    }
+}
